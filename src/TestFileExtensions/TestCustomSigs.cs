90036be8ae481d0f904ed8bbaab77034bc02f7d6
ignatandrei <ignatandrei@yahoo.com>
2021-10-26 22:45:40 +0300

    light bdd starting

--- src/TestFileExtensions/TestCustomSigs.cs
+++ src/TestFileExtensions/TestCustomSigs.cs
@@ -1,4 +1,7 @@
 ﻿using FluentAssertions;
+using LightBDD.Framework;
+using LightBDD.Framework.Scenarios;
+using LightBDD.XUnit2;
 using RecognizeCustomSigs_GCK;
 using RecognizeFileExtensionBL;
 using System;
@@ -9,10 +12,39 @@ using Xunit;
 
 namespace TestFileExtensions
 {
-    public class TestCustomSigs
+    [FeatureDescription(@"test signatures")]
+    [Label(nameof(TestCustomSigs))]
+    public class TestCustomSigs: FeatureFixture
     {
+        RecognizeFileExt r;
+        [Scenario]
+        [ScenarioCategory("TestSimple")]
+        public async void TestCHM1() 
+        {
+            await Runner
+               .AddSteps(Given_The_Recognizer)
+               .AddSteps(_ => Then_Should_Recognize_Extension("chm"))
+               //.AddAsyncSteps(
+               //    _ => When_The_User_Access_The_Url("/recordVisitors/AllVisitors5Min")
+               //)
+               //.AddSteps(
+               //    _ => Then_The_Response_Should_Contain("JeanIrvine")
+
+               //)
+               .RunAsync();
+
+        }
+        private void Given_The_Recognizer()
+        {
+            r = new RecognizeCustomSigs();
+        }
+        private void Then_Should_Recognize_Extension(string ext)
+        {
+            r.CanRecognizeExtension("chm").Should().BeTrue();
+        }
+
         [Fact]
-        public void TestCHM()
+        public void TestAll()
         {
             var r = new RecognizeCustomSigs();
 

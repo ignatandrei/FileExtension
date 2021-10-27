1bab83f59f3e7a25bd230cd63d0376f2f0cb601e
ignatandrei <ignatandrei@yahoo.com>
2021-10-27 08:31:42 +0300

    with roslyn

---
+++ src/RSCG_GCK/GCKGenerator.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace RSCG_GCK
+{
+    [Generator]
+    public class GCKGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var nameSpace = context.Compilation.Assembly.NamespaceNames.First();
+            var lines = File.ReadAllLines("customsigs_GCK.txt");
+            List<string> classes = new ();
+            foreach (var l in lines)
+            {
+                var data = l.Replace("\r", "").Replace("\n", "").Trim();
+                if (data.StartsWith("#"))//comment
+                    continue;
+                if (string.IsNullOrEmpty(data))
+                    continue;
+                var split = data.Split(',');
+                string ext = split[2].Trim();
+                ext = ext.Replace("|", "");
+                string className = $"RecognizeFromLine{ext}";
+                string content = $@"
+namespace {nameSpace} {{
+    public class {className}: RecognizeFromLineCustomsigs{{
+        public {className}(""{data}""): base(line)                
+
+    }}
+}}            
+";
+                context.AddSource(className + ".cs",content);
+                classes.Add(className);
+                //var r = new RecognizeFromLineCustomsigs(l);
+                //recognizes.Add(r);
+            }
+            var add = string.Join(Environment.NewLine, classes.Select(it => @$"recognizes.Add(""{it}"");"));
+            context.AddSource("RecognizeCustomSigs_GCK_recognizes.cs", $@"
+namespace RecognizeCustomSigs_GCK
+{{
+    public partial class RecognizeCustomSigs
+    {{
+        void AddRecognizers(){{
+                {add}
+                }}
+            
+");
+
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+
+        }
+    }
+}
--- src/RecognizeCustomSigs_GCK/RecognizeCustomSigs.cs
+++ src/RecognizeCustomSigs_GCK/RecognizeCustomSigs.cs
@@ -4,23 +4,24 @@ using System.IO;
 
 namespace RecognizeCustomSigs_GCK
 {
-    public class RecognizeCustomSigs: RecognizeFileExt
+    public partial class RecognizeCustomSigs: RecognizeFileExt
     {
+        partial void AddRecognizers();
         public RecognizeCustomSigs()
         {
+            AddRecognizers();
 
-
-            var lines = File.ReadAllLines("customsigs_GCK.txt");
-            foreach (var l in lines)
-            {
-                var data = l.Replace("\r", "").Replace("\n", "").Trim();
-                if (data.StartsWith("#"))//comment
-                    continue;
-                if (string.IsNullOrEmpty(data))
-                    continue;
-                var r = new RecognizeFromLineCustomsigs(l);
-                recognizes.Add(r);
-            }
+            //var lines = File.ReadAllLines("customsigs_GCK.txt");
+            //foreach (var l in lines)
+            //{
+            //    var data = l.Replace("\r", "").Replace("\n", "").Trim();
+            //    if (data.StartsWith("#"))//comment
+            //        continue;
+            //    if (string.IsNullOrEmpty(data))
+            //        continue;
+            //    var r = new RecognizeFromLineCustomsigs(l);
+            //    recognizes.Add(r);
+            //}
         }
         
     }

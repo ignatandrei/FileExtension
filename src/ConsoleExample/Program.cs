de44e506ed5bee28fa99a6ac8d68cecab427aeda
Andrei Ignat <ignatandrei@yahoo.com>
2021-11-02 14:32:10 +0200

    modified example

--- src/ConsoleExample/Program.cs
+++ src/ConsoleExample/Program.cs
@@ -12,19 +12,31 @@ namespace ConsoleExample
         {
             Console.WriteLine("Hello World!");
             var r = new RecognizerPlugin.RecognizePlugins();
-            foreach (var item in r.AllExtensions())
-            {
-                Console.WriteLine(item);
-            }
-            //find the sln
+            //foreach (var item in r.AllExtensions())
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //find the sln on the path
             string file = FindSlnToBeRecognized();
-            var fileExtension = Path.GetExtension(file);
-            var canRecognize = r.CanRecognizeExtension(fileExtension);
-            Console.WriteLine($"file {file} can be  recognized {canRecognize}");
             //found sln, now recognize
             var byts = await File.ReadAllBytesAsync(file);
-            var found = r.RecognizeTheFile(byts, fileExtension);
-            Console.Write($"file {file} is recognized {found}");
+
+            //find short way - directly
+            var foundDirectly = r.Is_SLN(byts);
+            Console.WriteLine($"file {file} is recognized {foundDirectly}");
+            // or long way
+            //find extension
+            var fileExtension = Path.GetExtension(file);
+            // find if there is a recognizer for the extension
+            var canRecognize = r.CanRecognizeExtension(fileExtension);
+            Console.WriteLine($"file {file} can be  recognized {canRecognize}");
+            if (canRecognize)
+            {
+                //now see if the content matches the bytes
+                var found = r.RecognizeTheFile(byts, fileExtension);
+                Console.Write($"file {file} is recognized {found}");
+            }
+            
             return;
         }
         static string FindSlnToBeRecognized()
